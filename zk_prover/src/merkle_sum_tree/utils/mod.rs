93934aa77b15a16e52294433f8a129b614ed66a6
Yagnadeep <yagnadeep.xo@gmail.com>
2024-02-27 22:37:34 +0530

    fuzz tests

--- zk_prover/src/merkle_sum_tree/utils/mod.rs
+++ zk_prover/src/merkle_sum_tree/utils/mod.rs
@@ -1,6 +1,6 @@
 mod build_tree;
-mod csv_parser;
-mod operation_helpers;
+pub mod csv_parser;
+pub mod operation_helpers;
 
 pub use build_tree::{build_leaves_from_entries, build_merkle_tree_from_leaves};
 pub use csv_parser::parse_csv_to_entries;
